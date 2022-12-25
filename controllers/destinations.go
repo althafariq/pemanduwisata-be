330e84125dde2cf77d22233f97b1aca40322a6ea
althafariq <althafariq04@gmail.com>
2022-12-25 13:08:22 +0700

    add get destination by id and upload image

--- controllers/destinations.go
+++ controllers/destinations.go
@@ -1,21 +1,21 @@
 package controllers
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/althafariq/pemanduwisata-be/models"
 	"github.com/gin-gonic/gin"
 )
-
-type CreateDestinationRequest struct {
-	Name        string `json:"name" binding:"required"`
-	Location    string `json:"location" binding:"required"`
-	Description string `json:"description" binding:"required"`
-	BudayaName  string `json:"budaya_name"`
-	BudayaDescription string `json:"budaya_description"`
-	PhotoPath   string `json:"photo_path"`
-}
 
 type CreateDestinationResponse struct {
 	ID 		int 		`json:"id"`
@@ -23,8 +23,8 @@ type CreateDestinationResponse struct {
 	SuccessResponse
 }
 
-type UpdateDestinationRequest struct {
-	ID 					int    `json:"id"`
+type DestinationRequest struct {
+	ID 					int    `json:"id"` //don't need when create new destination
 	Name        string `json:"name" binding:"required"`
 	Location    string `json:"location" binding:"required"`
 	Description string `json:"description" binding:"required"`
@@ -45,6 +45,11 @@ func (api *API) GetAllDestination(c *gin.Context) {
 	destination, err := api.destinationModels.GetAllDestinations()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	if len(destination) == 0 {
+		c.JSON(http.StatusOK, []string{}) //return empty array
 		return
 	}
 
@@ -85,57 +90,48 @@ func (api *API) GetAllDestination(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
-// func (api *API) GetDestination(c *gin.Context) {
-// 	// load all information about destination such as reviews, average rating, total review, and reviews
-// 	destinationID, err := strconv.Atoi(c.Query("id"))
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, ErrorResponse{Message: err.Error()})
-// 		return
-// 	}
+func (api *API) GetDestination(c *gin.Context) {
+	destinationID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse{Message: err.Error()})
+		return
+	}
 
-// 	destination, err := api.destinationModels.GetDestinationbyID(destinationID)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
-// 		return
-// 	}
+	destination, err := api.destinationModels.GetDestinationbyID(destinationID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+		return
+	}
 
-// 	avgRating, err := api.reviewModels.GetAverageRating(destinationID)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
-// 		return
-// 	}
+	avgRating, err := api.reviewModels.GetAverageRating(destinationID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+		return
+	}
 
-// 	totalReview, err := api.reviewModels.GetReviewCount(destinationID)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
-// 		return
-// 	}
+	totalReview, err := api.reviewModels.GetReviewCount(destinationID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+		return
+	}
 
-// 	reviewComment, err := api.reviewModels.GetReviewbyDestinationID(destinationID)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
-// 		return
-// 	}
+	destinationDetail := models.Destination{
+		ID: 									destination[0].ID,
+		Name: 								destination[0].Name,
+		Location: 						destination[0].Location,
+		Description: 					destination[0].Description,
+		BudayaName: 					destination[0].BudayaName,
+		BudayaDescription: 		destination[0].BudayaDescription,
+		PhotoPath: 						destination[0].PhotoPath,
+		AvgRating: 						avgRating,
+		TotalReview: 					totalReview,
+	}
 
-// 	destinationDetail := models.Destination{
-// 		ID: 									destination.ID,
-// 		Name: 								destination.Name,
-// 		Location: 						destination.Location,
-// 		Description: 					destination.Description,
-// 		BudayaName: 					destination.BudayaName,
-// 		BudayaDescription: 		destination.BudayaDescription,
-// 		PhotoPath: 						destination.PhotoPath,
-// 		AvgRating: 						avgRating,
-// 		TotalReview: 					totalReview,
-// 		Review: 							reviewComment[0],
-// 	}
+	c.JSON(http.StatusOK, destinationDetail)
+}
 
-// 	c.JSON(http.StatusOK, destinationDetail)
-// }
-
-// create destination
 func (api *API) CreateDestination(c *gin.Context) {
-	var req CreateDestinationRequest
+	var req DestinationRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, ErrorResponse{Message: err.Error()})
 		return
@@ -147,7 +143,6 @@ func (api *API) CreateDestination(c *gin.Context) {
 		Description: 				req.Description,
 		BudayaName:  				req.BudayaName,
 		BudayaDescription: 	req.BudayaDescription,
-		PhotoPath:   				req.PhotoPath,
 	})
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
@@ -163,9 +158,91 @@ func (api *API) CreateDestination(c *gin.Context) {
 	})
 }
 
-// update destination
+func (api *API) UploadImage(c *gin.Context) {
+	var (
+		destinationID int
+		err           error
+	)
+
+	if destinationID, err = strconv.Atoi(c.Param("id")); err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse{Message: "Invalid destination ID"})
+		return
+	}
+
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	folderPath := "media/destination"
+	err = os.MkdirAll(folderPath, os.ModePerm)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	files := form.File["images"]
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	for _, file := range files {
+		wg.Add(1)
+
+		go func(file *multipart.FileHeader) {
+			defer wg.Done()
+
+			defer func() {
+				if v := recover(); v != nil {
+					log.Println(v)
+					c.JSON(http.StatusInternalServerError, ErrorResponse{Message: "Internal Server Error"})
+					return
+				}
+			}()
+
+			uploadedFile, err := file.Open()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+				return
+			}
+
+			defer uploadedFile.Close()
+
+			unixTime := time.Now().UTC().UnixNano()
+			fileName := fmt.Sprintf("%d-%d-%s", destinationID, unixTime, strings.ReplaceAll(file.Filename, " ", ""))
+			fileLocation := filepath.Join(folderPath, fileName)
+			targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+				return
+			}
+
+			defer targetFile.Close()
+
+			if _, err := io.Copy(targetFile, uploadedFile); err != nil {
+				c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+				return
+			}
+
+			mu.Lock()
+			if err := api.destinationModels.InsertDestinationImage(destinationID, fileLocation); err != nil {
+				c.JSON(http.StatusInternalServerError, ErrorResponse{Message: err.Error()})
+				return
+			}
+			mu.Unlock()
+		}(file)
+	}
+
+	wg.Wait()
+
+	c.JSON(http.StatusOK, SuccessResponse{
+		Message: "Destination Images Uploaded",
+	})
+
+}
+
 func (api *API) UpdateDestination(c *gin.Context) {
-	var req UpdateDestinationRequest
+	var req DestinationRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, ErrorResponse{Message: err.Error()})
 		return
